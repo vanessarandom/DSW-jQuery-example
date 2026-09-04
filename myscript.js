44db8905baca465228ec286ced63ae701772bd04
vanessarandom <adriansoto72375@gmail.com>
2026-09-04 13:30:42 -0700

    adding code

--- myscript.js
+++ myscript.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
 		$("p").addClass("bright");
 		$("img").addClass("border");
 	});
+	$("#p1").click(function(){
+  $(".disappear").hide();
+	});
+	$("h1").mouseenter(function(){
+  $(this).css("background-color", "pink");
+});
+	$("h1").mouseleave(function(){
+  $(this).css("background-color", "lightblue");
+});
 });
